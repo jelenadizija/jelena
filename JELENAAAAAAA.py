8adb4ad8f46766483eb42e6b0a130de4fffa6e4e
jelenadizija <jelena.dizija@gmail.com>
2017-07-01 17:19:19 +0200

    Add files via upload

---
+++ JELENAAAAAAA.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  1 17:04:42 2017
+
+@author: admin
+"""
+
+telefonski_imenik = {"Paja Patak": 123456, "Mini Maus": 234567, "Šilja ":345678}
+vukajlija = {"sojanica": "Posna pljeskavica. Garantovano bez trihinele.", "jahanje": "Omiljena aktivnost šefova za koju je potrebno da radnik ima konjske živce.", "šef": "Čovek koji nema smisao za umor."}
+vrste_reči = {"imenice": ["polaznik", "seminar", "lingvistika", "Isidora"], "glagoli": ["slušati", "crtati", "jesti"], "zamenice": ["on", "ona", "ono"]}
+
+print(telefonski_imenik["Mini Maus"])
+print(telefonski_imenik.keys())
